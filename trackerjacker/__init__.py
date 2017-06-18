bc22f49009dd0cbcd963d603ad6419ee27186f1e
Caleb Madrigal <caleb.madrigal@gmail.com>
2017-06-18 17:45:16 -0400

    Updated to scapy3 0.21

--- trackerjacker/__init__.py
+++ trackerjacker/__init__.py
@@ -7,7 +7,7 @@ Finds and tracks wifi devices through raw 802.11 monitoring
 __author__ = "Caleb Madrigal"
 __email__ = "caleb.madrigal@gmail.com"
 __license__ = "MIT"
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 import trackerjacker
 

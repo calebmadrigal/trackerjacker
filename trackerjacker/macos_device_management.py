491757ed1ad1c24c009bd01f5931bedf5fc68c9e
Caleb Madrigal <caleb.madrigal@gmail.com>
2018-05-04 11:31:18 -0500

    Actual airport extreme wifi channels - still hardcoded :(

--- trackerjacker/macos_device_management.py
+++ trackerjacker/macos_device_management.py
@@ -110,9 +110,9 @@ def find_first_monitor_interface():
 
 
 def get_supported_channels(iface):
-    # Just a guess - TODO: get supported channels
-    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 36, 38, 40, 42, 44, 46, 48, 52, 54,
-            56, 58, 60, 62, 64, 100, 102, 104, 106]
+    # TODO: query supported channels
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108,
+            112, 116, 120, 124, 128, 132, 136, 140, 144, 149, 153, 157, 161, 165]
 
 
 def switch_to_channel(iface, channel_num):

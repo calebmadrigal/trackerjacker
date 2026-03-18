28229a2b2c508ff29abec7385bbfb68827972ab7
Caleb Madrigal <caleb.madrigal@gmail.com>
2026-03-18 15:30:24 -0400

    Minor update to setup.py

--- setup.py
+++ setup.py
@@ -53,11 +53,13 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: 3.15',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: System :: Networking',
         'Topic :: System :: Networking :: Monitoring',
         'Topic :: Security',
         'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS'
     ],
 )
